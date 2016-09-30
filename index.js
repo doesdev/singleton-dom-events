f13c750669f401eec362482d9af8a9a7ffb1aa5b
Andrew Carpenter <andrew@audioinhd.com>
2016-09-30 15:47:31 -0400

    Fix uglify breaking code

--- index.js
+++ index.js
@@ -7,24 +7,26 @@ module.exports = setEvent
 // Main
 function evtOn (el, evt, cb, capture) {
   if (!el || !(el.addEventListener || el.attachEvent)) return cb
-  evt = el.addEventListener ? evt : 'on' + evt
-  var callee = el.addEventListener || el.attachEvent
+  evt = el.addEventListener ? evt : ('on' + evt)
+  var callee = (el.addEventListener || el.attachEvent)
   callee.call(el, evt, cb, capture)
   return cb
 }
 
 function evtOff (el, evt, cb, capture) {
   if (!el || !(el.removeEventListener || el.detachEvent)) return cb
-  evt = el.addEventListener ? evt : 'on' + evt
-  var callee = el.removeEventListener || el.detachEvent
+  evt = el.addEventListener ? evt : ('on' + evt)
+  var callee = (el.removeEventListener || el.detachEvent)
   callee.call(el, evt, cb, capture)
   return cb
 }
 
 function setEvent (identifier, el, evt, cb, capture) {
-  var event = evtManager[identifier] = evtManager[identifier] || {el, evt, cb}
-  evtOff(event.el, event.evt, event.cb, capture || false)
-  event = evtManager[identifier] = {el, evt, cb}
-  evtOn(event.el, event.evt, event.cb, capture || false)
+  capture = capture === true
+  var crntEvt = {el: el, evt: evt, cb: cb}
+  var event = evtManager[identifier] = evtManager[identifier] || crntEvt
+  evtOff(event.el, event.evt, event.cb, capture)
+  event = evtManager[identifier] = crntEvt
+  evtOn(event.el, event.evt, event.cb, capture)
   return cb
 }
